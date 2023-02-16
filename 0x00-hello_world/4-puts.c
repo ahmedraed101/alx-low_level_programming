6d55288577286f042ccad47ff21c8c9adc8ebfdc
ahmed raed <araed2217@gmail.com>
2023-02-16 11:43:22 -0800

    fix betty issus

--- 0x00-hello_world/4-puts.c
+++ 0x00-hello_world/4-puts.c
@@ -1,7 +1,15 @@
 #include <stdio.h>
 
+/**
+ * main - Entry point
+ *
+ * Description: 'simple C code to print to stdio using puts (have new line)'
+ *
+ * Return: Always 0 (Success)
+*/
+
 int main(void)
 {
 	puts("\"Programming is like building a multilingual puzzle");
-	return(0);
+	return (0);
 }
