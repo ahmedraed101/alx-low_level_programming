e5e5799a35d99accd8920d8ab4e13a07e36ff483
ahmed raed <araed2217@gmail.com>
2023-02-17 14:43:30 -0800

    reverse alphabet

---
+++ 0x01-variables_if_else_while/7-print_tebahpla.c
@@ -0,0 +1,26 @@
+#include <stdlib.h>
+#include <time.h>
+#include <stdio.h>
+/* more headers goes there */
+
+/**
+ * main - prints the alphabet in lower
+ *
+ * Return: Always 0 (sucess)
+*/
+
+/* betty style doc for function main goes there */
+int main(void)
+{
+	char letter;
+	int start = 122;
+
+	while (start > 96)
+	{
+		letter = start;
+		putchar(letter);
+		--start;
+	}
+	putchar('\n');
+	return (0);
+}
