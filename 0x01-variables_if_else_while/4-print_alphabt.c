c9cdf5b74cc767aca441d52ec31096a9f934c94d
ahmed raed <araed2217@gmail.com>
2023-02-17 14:32:23 -0800

    skip e and q

---
+++ 0x01-variables_if_else_while/4-print_alphabt.c
@@ -0,0 +1,31 @@
+#include <stdlib.h>
+#include <time.h>
+#include <stdio.h>
+/* more headers goes there */
+
+/**
+ * main - prints the alphabet in lower except q and e
+ *
+ * Return: Always 0 (sucess)
+*/
+
+/* betty style doc for function main goes there */
+int main(void)
+{
+	char letter;
+	int lower = 97;
+
+	while (lower < 123)
+	{
+		letter = lower;
+		++lower;
+		if (letter == 'e' || letter == 'q')
+		{
+			continue;
+		}
+		putchar(letter);
+		
+	}
+	putchar('\n');
+	return (0);
+}
