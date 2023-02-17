0865b68e66567d0f08e36db5831f358b661b0475
ahmed raed <araed2217@gmail.com>
2023-02-17 13:54:28 -0800

    lower and upper alphabet

---
+++ 0x01-variables_if_else_while/3-print_alphabets.c
@@ -0,0 +1,33 @@
+#include <stdlib.h>
+#include <time.h>
+#include <stdio.h>
+/* more headers goes there */
+
+/**
+ * main - prints the alphabet in lower
+ *
+ * Return: Always 0 (sucess)
+*/
+
+/* betty style doc for function main goes there */
+int main(void)
+{
+	char letter;
+	int lower = 97;
+	int upper = 65;
+
+	while (lower < 123)
+	{
+		letter = lower;
+		putchar(letter);
+		++lower;
+	}
+	while (upper < 91)
+	{
+		letter = upper;
+		putchar(letter);
+		++upper;
+	}
+	putchar('\n');
+	return (0);
+}
