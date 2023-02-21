52dd1dfbb7414f10052259ad8a94d519fb031b90
ahmed raed <araed2217@gmail.com>
2023-02-21 14:33:21 -0800

    print alpha x10

---
+++ 0x02-functions_nested_loops/2-print_alphabet_x10.c
@@ -0,0 +1,21 @@
+#include "main.h"
+
+/**
+ * print_alphabet_x10 - prints the lower case alphabet chars 10 times
+ *
+ * Return: Always 0
+*/
+
+int print_alphabet_x10(void)
+{
+	char letter;
+	int i;
+
+	for (i = 0; i < 10; ++i)
+	{
+		for (letter = 'a'; letter <= 'z'; ++letter)
+			_putchar(letter);
+		_putchar('\n');
+	}
+	return (0);
+}
