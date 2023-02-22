5842e0bd75864561baac31ce84d64574910d9769
ahmed raed <araed2217@gmail.com>
2023-02-21 16:50:16 -0800

    add two numbers

---
+++ 0x02-functions_nested_loops/10-add.c
@@ -0,0 +1,14 @@
+#include "main.h"
+
+/**
+ * add - adds two numbers
+ * @a: the first number to be added
+ * @b: the second number to be added
+ *
+ * Return: the sum of a & b
+*/
+
+int add(int a, int b)
+{
+	return (a + b);
+}
