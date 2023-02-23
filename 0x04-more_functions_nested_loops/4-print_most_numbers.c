bca775ed953d2ca4bf8a6780d7bb6e1edec1655c
ahmed raed <araed2217@gmail.com>
2023-02-23 06:05:29 -0800

    print 0-9 cd ~,4

---
+++ 0x04-more_functions_nested_loops/4-print_most_numbers.c
@@ -0,0 +1,17 @@
+#include "main.h"
+
+/**
+ * print_most_numbers - prints numbers [0-9] but (2,4) + \n
+ *
+ * Return: no return
+*/
+
+void print_most_numbers(void)
+{
+	int i;
+
+	for (i = '0'; i <= '9'; i++)
+		if (i != '2' && i != '4')
+			_putchar(i);
+	_putchar('\n');
+}
