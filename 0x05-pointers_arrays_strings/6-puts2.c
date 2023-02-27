6d95f2420fc0865dc5ca5cf7674df6382822dbca
ahmedraed101 <araed2217@gmail.com>
2023-02-28 00:28:14 +0200

    print every other

---
+++ 0x05-pointers_arrays_strings/6-puts2.c
@@ -0,0 +1,25 @@
+#include "main.h"
+/**
+* puts2 - this function print every other char of the string
+* @str: the string to be printed every other char of
+*
+* Return: no thing
+*/
+void _puts(char *str)
+{
+	int i, p;
+
+	i = 0;
+	p = 1;
+	while (str[i] != '\0')
+	{
+		if (p == 1)
+		{
+			p = 0;
+			_putchar(str[i]);
+		} else
+			p = 1;
+		i++;
+	}
+	_putchar('\n');
+}
