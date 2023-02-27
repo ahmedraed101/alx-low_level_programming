1cdbadc2b6c0657923aebfc8cdf17f743be9b658
ahmedraed101 <araed2217@gmail.com>
2023-02-28 00:19:07 +0200

    fix

--- 0x05-pointers_arrays_strings/5-rev_string.c
+++ 0x05-pointers_arrays_strings/5-rev_string.c
@@ -13,7 +13,7 @@ void rev_string(char *s)
 	len = 0;
 	while (s[len] != '\0')
 		len++;
-	for (i = 0; i <= (len / 2); i++)
+	for (i = 0; i < (len / 2); i++)
 	{
 		temp = s[len - 1 - i];
 		s[len - 1 - i] = s[i];
