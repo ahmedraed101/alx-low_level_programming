5fe04f46edc4e80f700a557babffd62ca4fd5d10
ahmedraed101 <araed2217@gmail.com>
2023-02-27 23:09:52 +0200

    0-reset_to_98.c

---
+++ 0x05-pointers_arrays_strings/0-reset_to_98.c
@@ -0,0 +1,11 @@
+/**
+* reset_to_98 - takes an pointer n and change value of that location
+* @n: is the pointer to the var to set 98
+* 
+* Return: no thing
+*/
+
+void reset_to_98(int *n)
+{
+	*n = 98;
+}
