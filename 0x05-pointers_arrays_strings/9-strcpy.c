b2800e8de017916715818999c09870c76df0dbc3
ahmedraed101 <araed2217@gmail.com>
2023-02-28 01:09:49 +0200

    copy string

---
+++ 0x05-pointers_arrays_strings/9-strcpy.c
@@ -0,0 +1,21 @@
+#include "main.h"
+/**
+* *_strcpy - copy array to another
+* @src: the array to copy from
+* @dest: the array to copy to
+*
+* Return: no thing
+*/
+char *_strcpy(char *dest, char *src)
+{
+	int i;
+
+	i = 0;
+	while (1 == 1)
+	{
+		dest[i] = src[i];
+		if (src[i])
+			break;
+		i++;
+	}
+}
