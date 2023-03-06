90445aafaabfd1c4073611e2c0acc3c1740cf81e
ahmedraed101 <araed2217@gmail.com>
2023-03-06 14:22:07 +0200

    memset

---
+++ 0x07-pointers_arrays_strings/0-memset.c
@@ -0,0 +1,17 @@
+/**
+* _memset - fills the first n bytes of the memorey of array s with contanct b
+* @s: the array to set it's memory values
+* @b: the constant to fill the array with
+* @n: the limit of the bytes of the array
+*
+* Return: pointer to the new array
+*/
+
+char *_memset(char *s, char b, unsigned int n)
+{
+	int i;
+
+	for (i = 0; i < n; i++)
+		s[i] = b;
+	return s;
+}
---
+++ 0x07-pointers_arrays_strings/README.md
@@ -0,0 +1,7 @@
+# 0x07-pointers_arrays_strings
+
+## Scripts
+
+- 0-memset.c
+	function that fills memory with a constant byte (memset clone)
+
