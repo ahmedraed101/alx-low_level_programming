4ca9620330a391ff83459e5ef3b7aa8547ea779c
ahmedraed101 <araed2217@gmail.com>
2023-03-06 14:33:33 +0200

    memcpy

---
+++ 0x07-pointers_arrays_strings/1-memcpy.c
@@ -0,0 +1,17 @@
+/**
+* _memcpy - compies n number of bytes from src array to dest array
+* @dest: the array to copy the src array to
+* @src: the source array to copy into dest
+* @n: the limit of the bytes to copy
+*
+* Return: pointer to the new array
+*/
+
+char *_memset(char *dest, char *src, unsigned int n)
+{
+	unsigned int i;
+
+	for (i = 0; i < n; i++)
+		dest[i] = src[i];
+	return (dest);
+}
