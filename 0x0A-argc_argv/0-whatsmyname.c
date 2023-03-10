87e3e22ae45e24f1db6f627f736e27bc6f6ee395
ahmedraed101 <araed2217@gmail.com>
2023-03-10 21:34:50 +0200

    print name

---
+++ 0x0A-argc_argv/0-whatsmyname.c
@@ -0,0 +1,17 @@
+#include "main.h"
+
+/**
+* main - prints its name
+* @argc: argument count
+* @argv: argument vector
+*
+* Return: always 0
+*/
+
+int main(int argc, char *argv[])
+{
+	(void) argc;
+	printf("%s\n");
+
+	return (0);
+}
---
+++ 0x0A-argc_argv/README.md
@@ -0,0 +1,8 @@
+# 0x0A-argc_argv
+
+## Scripts
+
+- 0-whatsmyname.c:
+	program that prints its name, followed by a new line.
+
+-
