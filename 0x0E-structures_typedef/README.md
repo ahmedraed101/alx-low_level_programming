5f598e8ef175d8804cebb62c390e6dbf4397ac69
ahmedraed101 <araed2217@gmail.com>
2023-03-21 01:43:44 +0200

    dog.h

---
+++ 0x0E-structures_typedef/README.md
@@ -0,0 +1,8 @@
+# 0x0E-structures_typedef
+
+## Scripts
+
+- dog.h:
+	Define a new type struct dog
+
+- 
---
+++ 0x0E-structures_typedef/dog.h
@@ -0,0 +1,19 @@
+#ifndef DOG_H
+#define DOG_H
+
+/**
+ * struct dog - the dog's info
+ * @name: first info
+ * @age: second info
+ * @owner: third info
+ *
+ * Description: it combines 3 infos about dog
+*/
+
+struct dog
+{
+	char *name;
+	float age;
+	char *owner;
+}
+#endif
