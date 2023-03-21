67b40c53556a409ff77992ae08df79eede2dfc15
ahmedraed101 <araed2217@gmail.com>
2023-03-21 02:17:48 +0200

    typedef 2

--- 0x0E-structures_typedef/dog.h
+++ 0x0E-structures_typedef/dog.h
@@ -10,19 +10,12 @@
  * Description: it combines 3 infos about dog
 */
 
-struct dog
+typedef struct dog
 {
 	char *name;
 	float age;
 	char *owner;
-};
-
-/**
- * dog_t - typedef for struct dog
- */
-
-typedef struct dog dog_t;
-
+} dog_t;
 
 void init_dog(struct dog *d, char *name, float age, char *owner);
 void print_dog(struct dog *d);
