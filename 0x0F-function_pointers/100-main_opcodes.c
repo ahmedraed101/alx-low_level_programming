3737cb29965ec0115a6989959acaef5c84d059c2
ahmedraed101 <araed2217@gmail.com>
2023-03-22 21:57:31 +0200

    opcodes

---
+++ 0x0F-function_pointers/100-main_opcodes.c
@@ -0,0 +1,35 @@
+#include <stdio.h>
+#include <stdlib.h>
+/**
+ * main - prints the opcodes if its own
+ * @argc: argument count
+ * @argv: argument vector
+ *
+ * Return: 0 if success, 1 if argc not right, 2 if bytes is negative
+ */
+
+int main(int argc, char **argv)
+{
+	int i, bytes;
+	char *opcodes;
+
+	if (argc != 2)
+	{
+		printf("Error\n");
+		return (1);
+	}
+	bytes = atoi(argv[1]);
+	if (bytes < 0)
+	{
+		printf("Error\n");
+		return (2);
+	}
+	opcodes = (char *)main;
+
+	for (i = 0; i < bytes; i++)
+	{
+		printf("%02hhx", opcodes[i]);
+	}
+	printf("\n");
+	return (0);
+}
--- 0x0F-function_pointers/README.md
+++ 0x0F-function_pointers/README.md
@@ -10,3 +10,9 @@
 
 - 2-int_index.c:
 	a function that searches for an integer
+
+- 3-main.c, 3-get_op_func.c, 3-op_functions, 3-calc.h:
+	a program that preforms simple operations on two numbers
+
+- 100-main_opcodes.c:
+	a program that prints the opcodes of its own main function
