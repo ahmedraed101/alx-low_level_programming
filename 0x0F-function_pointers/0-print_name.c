5eb9f3db5d2e6ca041a08099f9df4f14a1cfa68a
ahmedraed101 <araed2217@gmail.com>
2023-03-22 16:14:47 +0200

    NULL include

--- 0x0F-function_pointers/0-print_name.c
+++ 0x0F-function_pointers/0-print_name.c
@@ -1,4 +1,4 @@
-
+#include "function_pointers.h"
 /**
  * print_name - prints a given string
  * @name: a array of char
--- 0x0F-function_pointers/function_pointers.h
+++ 0x0F-function_pointers/function_pointers.h
@@ -1,6 +1,8 @@
 #ifndef FUNCTION_POINTERS_H
 #define FUNCTION_POINTER_H
 
+#include <stdlib.h>
+
 int _putchar(char);
 void print_name(char *name, void (*f)(char *));
 #endif
