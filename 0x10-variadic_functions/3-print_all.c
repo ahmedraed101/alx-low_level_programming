13284f375f3ae2c6b73eff857200b006c994d69d
ahmedraed101 <araed2217@gmail.com>
2023-03-23 17:56:16 +0200

    print all

---
+++ 0x10-variadic_functions/3-print_all.c
@@ -0,0 +1,50 @@
+#include "variadic_functions.h"
+#include <stdio.h>
+/**
+ * print_all - function that prints anything
+ * @format: an array of types of arguments
+ *
+ * Return: nothing
+*/
+
+void print_all(const char * const format, ...)
+{
+	va_list args;
+	char *str, *separator = "";
+	unsigned int i;
+
+	va_start(args, format);
+	if (format)
+	{
+		i = 0;
+		while (format[i] != '\0')
+		{
+			switch (format[i])
+			{
+				case 'c':
+					printf("%s%c", separator, va_arg(args, int));
+					break;
+				case 'i':
+					printf("%s%d", separator, va_arg(args, int));
+					break;
+				case 'f':
+					printf("%s%f", separator, va_arg(args, double));
+					break;
+				case 's':
+					str = va_arg(args, char*);
+					if (str == NULL)
+						str = "(nil)";
+					else
+						printf("%s%s", separator, str);
+					break;
+				default:
+					i++;
+					continue;
+			}
+			separator = ", ";
+			i++;
+		}
+	}
+	printf("\n");
+	va_end(args);
+}
--- 0x10-variadic_functions/README.md
+++ 0x10-variadic_functions/README.md
@@ -3,7 +3,13 @@
 ## Scripts
 
 - 0-sum_them_all.c:
-	a function that returns the sum of all its parameters
+	a variadic function that returns the sum of all its parameters
 
 - 1-print_numbers.c:
-	a function that prints the numbers, followd by a new line.
+	a variadic function that prints the numbers given followd by a new line.
+
+- 2-print_strings.c:
+	a variadic function that prints strings given followd by a new line.
+
+- 3-print_all.c:
+	a variadic function that prints all given arguments followd by a new line
