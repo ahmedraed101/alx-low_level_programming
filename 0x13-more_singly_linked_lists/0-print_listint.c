9f44dbe8131e8b49f3801809b5d28bf618f53ca7
ahmedraed101 <araed2217@gmail.com>
2023-04-03 17:24:35 +0200

    try fix 0

--- 0x13-more_singly_linked_lists/0-print_listint.c
+++ 0x13-more_singly_linked_lists/0-print_listint.c
@@ -2,19 +2,19 @@
 
 /**
  * print_listint - function that prints a listint_t
- * @head: the head node of the listint_t list
+ * @h: the head node of the listint_t list
  *
  * Return: the number of nodes printed
  */
 
-size_t print_listint(const listint_t *head)
+size_t print_listint(const listint_t *h)
 {
 	size_t i = 0;
 
-	while (head)
+	while (h)
 	{
-		printf("%d\n", head->n);
-		head = head->next;
+		printf("%d\n", h->n);
+		h = h->next;
 		i++;
 	}
 	return (i);
