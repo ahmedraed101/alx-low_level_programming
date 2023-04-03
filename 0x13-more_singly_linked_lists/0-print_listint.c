5125ba153fb6b3f505e929ccdc41acf4524ee4d1
ahmedraed101 <araed2217@gmail.com>
2023-04-03 17:29:48 +0200

    try -2 lines

--- 0x13-more_singly_linked_lists/0-print_listint.c
+++ 0x13-more_singly_linked_lists/0-print_listint.c
@@ -3,10 +3,8 @@
 /**
  * print_listint - function that prints a listint_t
  * @h: the head node of the listint_t list
- *
  * Return: the number of nodes printed
  */
-
 size_t print_listint(const listint_t *h)
 {
 	size_t i = 0;
