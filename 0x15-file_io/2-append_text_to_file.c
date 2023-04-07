e0ab55214171faa9e34ee67f049f66ae3e2a6c7f
ahmedraed101 <araed2217@gmail.com>
2023-04-07 11:47:37 +0200

    fix append

--- 0x15-file_io/2-append_text_to_file.c
+++ 0x15-file_io/2-append_text_to_file.c
@@ -18,6 +18,8 @@ int append_text_to_file(const char *filename, char *text_content)
 	if (fd == -1)
 		return (-1);
 
+	if (text_content == NULL)
+		return (1);
 
 	while (*(text_content + n) != '\0')
 		n++;
