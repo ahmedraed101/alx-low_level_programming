4dc91c390f17db6e3c30a81daa915f928b7149dd
ahmedraed101 <araed2217@gmail.com>
2023-04-07 14:02:18 +0200

    fix bin to uint

--- 0x14-bit_manipulation/0-binary_to_uint.c
+++ 0x14-bit_manipulation/0-binary_to_uint.c
@@ -1,25 +1,27 @@
 #include "main.h"
 
 /**
- * binary_to_unit - converts binary number to an unsigned int
+ * binary_to_uint - converts binary number to an unsigned int
  * @b: pionter to string of one bit
  *
  * Return: converted number or 0 if any char at @b != 0|1
  */
 
-unsigned int binary_to_unit(const char *b)
+unsigned int binary_to_uint(const char *b)
 {
-	unsigned int i, dec;
+	unsigned int i = 0;
+	unsigned int dec = 0;
 
 	if (b == NULL)
 		return (0);
 	while (*(b + i) != '\0')
 	{
-		if (*(b + i) != '0' && (*b + i) != '1')
+		if (*(b + i) != '0' && *(b + i) != '1')
 			return (0);
 		dec <<= 1;
 		if (*(b + i) == '1')
 			dec ^= 1;
 		i++;
 	}
+	return (dec);
 }
--- 0x14-bit_manipulation/main.h
+++ 0x14-bit_manipulation/main.h
@@ -2,6 +2,7 @@
 #define MAIN_H
 
 #include <stdlib.h>
-unsigned int binary_to_unit(const char *b);
+unsigned int binary_to_uint(const char *b);
+void print_binary(unsigned long int n);
 
 #endif
