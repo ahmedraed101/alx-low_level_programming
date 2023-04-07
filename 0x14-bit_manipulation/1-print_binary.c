24b6f06d9f5e183601e259e5c7828f1ad85a1801
ahmedraed101 <araed2217@gmail.com>
2023-04-07 14:15:22 +0200

    fix 1

--- 0x14-bit_manipulation/1-print_binary.c
+++ 0x14-bit_manipulation/1-print_binary.c
@@ -8,13 +8,10 @@
 
 void _shift_right(unsigned long int n)
 {
-	char c;
-
 	if (n == 0)
 		return;
 	_shift_right(n >> 1);
-	c = (n & 1) + '0';
-	printf("%c", c);
+	putchar((n & 1) + '0');
 }
 
 
@@ -28,7 +25,7 @@ void _shift_right(unsigned long int n)
 void print_binary(unsigned long int n)
 {
 	if (n == 0)
-		printf("0");
+		putchar('0');
 	else
 		_shift_right(n);
 }
