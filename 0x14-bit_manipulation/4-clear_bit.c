c392fb506606f28a64776c29546fbb1ea0241161
ahmedraed777 <araed2217@gmail.com>
2023-07-07 01:16:31 +0200

    clear_bit func

--- 0x14-bit_manipulation/4-clear_bit.c
+++ 0x14-bit_manipulation/4-clear_bit.c
@@ -2,18 +2,18 @@
 
 /**
  *
- * set_bit - a function that allow you to set a bit at certain index to one
+ * clear_bit - a function that allow you to set a bit at certain index to zero
  * @n: the number to change it's bits
  * @index: the index of the bit to change
  *
  * Return: 1 on success and -1 on failure
  */
 
-int set_bit(unsigned long int *n, unsigned int index)
+int clear_bit(unsigned long int *n, unsigned int index)
 {
 	if (index > 63)
 		return (-1);
 
-	*n = ((1 << index) | *n);
+	*n = (~(1 << index) & *n);
 	return (1);
 }
