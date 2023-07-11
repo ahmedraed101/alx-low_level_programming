62906bfed3b3404778e1f55ef2ba75ceab510f41
ahmedraed777 <araed2217@gmail.com>
2023-07-11 02:12:03 +0200

    -useless checks

--- 0x15-file_io/0-read_textfile.c
+++ 0x15-file_io/0-read_textfile.c
@@ -11,36 +11,20 @@
 
 ssize_t read_textfile(const char *filename, size_t letters)
 {
-	char *buffer = malloc(sizeof(char) * letters);
+	char *buffer;
 	ssize_t read_letters = 0;
 	int filep;
 
-	if (filename == NULL)
-	{
-		free(buffer);
-		return (0);
-	} else if (buffer == NULL)
-		return (0);
 
 	filep = open(filename, O_RDONLY);
 	if (filep == -1)
-	{
-		free(buffer);
 		return (0);
-	}
 
+	buffer = malloc(sizeof(char) * letters);
 	read_letters = read(filep, buffer, letters);
-	if (read_letters == -1)
-	{
-		free(buffer);
-		close(filep);
-		return (0);
-	}
-
 	read_letters = write(1, buffer, read_letters);
+
 	free(buffer);
 	close(filep);
-	if (read_letters == -1)
-		return (0);
 	return (read_letters);
 }
