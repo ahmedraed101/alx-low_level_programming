642f362d9c38a0fdfef442bd48affd7f3a089f8b
ahmedraed777 <araed2217@gmail.com>
2023-08-17 21:54:19 +0200

    init

---
+++ 0x17-doubly_linked_lists/0-print_dlistint.c
@@ -0,0 +1,21 @@
+#include "lists.h"
+
+/**
+ * print_dlistint - print a coubly linked list
+ * @h: the head node
+ *
+ * Return: the size of the list
+*/
+
+size_t print_dlistint(const dlistint_t *h)
+{
+	size_t size = 0;
+
+	while (h != NULL)
+	{
+		printf("%d\n", h->n);
+		h = h->next;
+		size++;
+	}
+	return size
+}
---
+++ 0x17-doubly_linked_lists/lists.h
@@ -0,0 +1,23 @@
+#ifndef LISTS_H
+#define LISTS_H
+#include <stdlib.h>
+#include <stdio.h>
+
+/**
+ * struct dlistint_t - doubly linked list
+ * @n: integer
+ * @next: points to the next node
+ * @prev: points to the prev node
+ *
+ * Description: doubly linked list node structure
+*/
+typedef struct dlistint_t
+{
+	int n;
+	dlistint_t *next;
+	dlistint_t *prev;
+} dlistint_t;
+
+size_t print_dlistint(const dlistint_t *h);
+
+#endif
